044a116e75912306e0f820cc86eb8e47741b6244
Tanishqdhuriya122 <tanishqdhuriya@gmail.com>
2025-10-10 23:18:21 +0530

    add register Controller and bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connecDB = async() => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
-        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.host}`);
+        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`);
     }catch(error) {
         console.log("Mongodb connnection error" , error)
         process.exit(1)
